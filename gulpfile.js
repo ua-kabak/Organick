f6fad05e72f79fedf75dddd8c1a3faeb171ea8ae
ua-kabak <kabak05012001@gmail.com>
2022-08-08 13:57:46 +0300

    finished project

--- gulpfile.js
+++ gulpfile.js
@@ -78,7 +78,6 @@ function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
     'node_modules/slick-carousel/slick/slick.js',
-    'node_modules/mixitup/dist/mixitup.js',
     'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
     'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
     'node_modules/slick-lightbox/dist/slick-lightbox.js',
